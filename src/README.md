8ca510f04cd50021f4fefa5492cd5eee4aac5f7f
Dinara-Ribeiro <dinaraf.waltrich@gmail.com>
2024-07-29 13:09:46 -0300

    Desafio completo

---
+++ src/README.md
@@ -0,0 +1,40 @@
+# Desafio Classificador de nível de herói💥
+![alt text](desafio-felipao.png)
+
+
+
+
+
+**O que deve ser utilizado** ❓
+
+- Variáveis ✔
+- Operadores ✔
+- Laços de repetição ✔
+- Estruturas de decisão ✔
+
+## Objetivo:📌
+- Crie uma variável para armazenar o nome e a quantidade  de experiência(XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor que 1.000 = Ferro
+
+Se XP for entre 1.001 e 2.000 = Bronze 
+
+Se XP for entre 2.001 e 5.000 = Prata Ouro
+
+Se XP  for entre 5.001 e 7.000 = Ouro
+
+Se XP for entre 7.001 e 8.000 = Platina 
+
+Se XP for entre 8.001 e 9.000 = Ascendente
+
+Se XP for ente 9.001 e 10.000 = Imortal
+
+Se XP for maior ou igual a 10.000 = Radiante
+
+
+## Saída:🎯
+Ao final deve se exibir uma mensagem:
+
+"O Herói de nome **{nome}** está no nível de **{nível}**."
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-let nome = "felipe";
-console.log("nome");
+// Nome/xp  no formato lista, declarando heróis
+let herois = [
+    { nome: "Dinara", xp: 5000 },
+    { nome: "Rodrigo", xp: 7000 },
+    { nome: "Ricardo", xp: 9000 },
+    { nome: "Pai", xp: 12000}
+  ];
+// Verificar cada herói da lista acima
+  for (let heroi of herois) {
+    let nome = heroi.nome; // guarda nome
+    let xp = heroi.xp; // guarda xp
+    let nivel; // guarda nivel
+  
+//  Verifica o nível do herói através do xp
+    if (xp < 1000) {
+      nivel = "Ferro";
+    } else if (xp <= 2000) {
+      nivel = "Bronze";
+    } else if (xp <= 5000) {
+      nivel = "Prata";
+    } else if (xp <= 7000) {
+      nivel = "Ouro";
+    } else if (xp <= 8000) {
+      nivel = "Platina";
+    } else if (xp <= 9000) {
+      nivel = "Ascendente";
+    } else if (xp <= 10000) {
+      nivel = "Imortal";
+    } else {
+      nivel = "Radiante";
+    }
+// Retorna mensagem com nome e nível baseado no que foi analisado 
+    console.log("O Herói de nome " + nome + "está no nível de " + nivel + ".");
+  }
